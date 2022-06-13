34eded16234dbd3cbad02d21b4c742e82bca9ed6
LincolnBuilder <Lincoln.builder@gmail.com>
2022-06-13 11:12:14 -0700

    Sneaky animation
    Made sneaky animations work. Still need to find a way to make it less glitchy.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -15,6 +15,11 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+         animation();
+    }
+    void animation()
+    {
+        //movement animation
         if (Input.GetKey("w"))
         {
             animator.SetBool("isWalking", true);
@@ -34,6 +39,38 @@ public class Player : MonoBehaviour
         else
         {
             animator.SetBool("isWalking", false);
+        }
+        //sneaky
+        if (Input.GetKey("left shift"))
+        {
+            animator.SetBool("isSneaky", true);
+            if (Input.GetKey("w"))
+            {
+                animator.SetBool("sneakyWalkForward", true);
+            }
+            else if (Input.GetKey("a"))
+            {
+                animator.SetBool("sneakyWalkLeft", true);
+            }
+            else if (Input.GetKey("s"))
+            {
+                animator.SetBool("sneakyWalkBack", true);
+            }
+            else if (Input.GetKey("d"))
+            {
+                animator.SetBool("sneakyWalkRight", true);
+            }
+            else
+            {
+                animator.SetBool("sneakyWalkForward", false);
+                animator.SetBool("sneakyWalkLeft", false);
+                animator.SetBool("sneakyWalkBack", false);
+                animator.SetBool("sneakyWalkRight", false);
+            }
+        }
+        else
+        {
+            animator.SetBool("isSneaky", false);
         }
     }
 }
